1a42c309d786b3a0337c4b5a3e232832fa85084a
ElPedron <pedrohenriquesilva@aluno.senai.br>
2023-12-16 18:36:49 -0300

    feat: create function controller

--- src/controllers/users/user.controller.js
+++ src/controllers/users/user.controller.js
@@ -1,3 +1,4 @@
+import e from "express";
 import Users from "../../models/users/users";
 
 //instancia a lista
@@ -27,4 +28,44 @@ export const getUserById = (req, res) => {
     return res.status(400).send({
         message: 'invalid_id'
     });
+}
+
+export const postUser = (req, res) => {
+    const { name, email, password, cpf, telephone, birthdate, balance } = req.body;
+
+    const errors = [];
+
+    if(!name || !email || !password || !cpf || !telephone || !birthdate || !balance) {
+        errors.push('complete_all_fields');
+    }
+
+    if(name.length < 3 && name.length > 100) {
+        errors.push('invalid_name');
+    }
+
+    if(email.length < 8 && email.length > 75) {
+        errors.push('invalid_name');
+    }
+
+    if(password.length < 8) {
+        errors.push('invalid_password');
+    }
+
+    if(cpf.length < 12 && cpf.length > 12 && typeof(cpf) != 'number') {
+        errors.push('invalid_cpf');
+    }
+
+    if(typeof(telephone) != 'number') {
+        errors.push('invalid_telephone');
+    }
+
+    if(typeof(balance) != 'number' && balance < 0) {
+        errors.push('invalid_balance');
+    }
+
+    errors.length ? (
+        return res.status(400).send({ errors });
+    ) : (
+        const user 
+    )
 }

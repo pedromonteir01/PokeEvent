21f41e6073951dd869117221202f7bc6aec00555
ElPedron <pedrohenriquesilva@aluno.senai.br>
2023-12-16 17:59:54 -0300

    feat: getAllUsers and getUserById method

--- src/models/users/users.js
+++ src/models/users/users.js
@@ -4,4 +4,13 @@ export default class Users {
     constructor() {
         this.users = [];
     }
+
+    getAllUsers() {
+        return this.users;
+    }
+
+    getUserById(id) {
+        const user = this.users.filter((user) => user.id == id);
+        return user;
+    }
 }

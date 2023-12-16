9791d1cd5c2d66511a3fbe6608910d1116193b0e
ElPedron <pedrohenriquesilva@aluno.senai.br>
2023-12-16 18:27:33 -0300

    feat: getUserById function controller

--- src/controllers/users/user.controller.js
+++ src/controllers/users/user.controller.js
@@ -8,10 +8,23 @@ export const getAllUsers = (req, res) => {
     const users = usersList.getAllUsers();
     //verifica se existe usuários
     if(users.length) {
-        return res.status(200).send({ users, quantity: users.length })
+        return res.status(200).send({ users, quantity: users.length });
     }
 
     return res.status(200).send({
         message: 'no_users_registered'
-    })
+    });
+}
+
+export const getUserById = (req, res) => {
+    const { id } = req.params;
+    const user = usersList.getUserById(id);
+
+    if(user) {
+        return res.status(200).send(user);
+    }
+
+    return res.status(400).send({
+        message: 'invalid_id'
+    });
 }

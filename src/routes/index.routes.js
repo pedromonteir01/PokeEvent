10020c57ef1735ec1fdb62eeb0d94d0c127d53c9
ElPedron <pedrohenriquesilva@aluno.senai.br>
2023-12-17 11:33:59 -0300

    feat: router get

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,4 +2,10 @@ import { Router } from "express";
 
 const router = Router();
 
+router.get("/", (req, res) => {
+    return res.status(200).send({
+        message: 'server ok'
+    });
+});
+
 export default router;
